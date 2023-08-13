0db6b98aad770eb5f86ac90825cb977b0f04a784
Paul Cho <chosung2loud@gmail.com>
2023-08-13 13:11:22 -0700

    using catmullrom to get the birds path trajectory

--- src/app/component/effects/seagull.tsx
+++ src/app/component/effects/seagull.tsx
@@ -10,6 +10,7 @@ import {
    Quaternion,
    Matrix4,
    CatmullRomCurve3,
+   LineBasicMaterial,
 } from 'three';
 
 // have to establish width -- what is the width and if the velocity is going fast
@@ -23,121 +24,149 @@ const Seagull = ({ position, count = 10 }: { position: Vector3; count?: number }
    const gltf = useGLTF('/glb/bird.glb');
    const birdRef = useRef<Mesh>(null!);
 
-   const sigma = 0.1; // Standard deviation for the Gaussian random walk
-   let theta = 0;
-   const radius = 6;
-   const radiusX = radius * 2;
-
-   const previousPosition = useMemo(() => new Vector3(), []);
+   // the radius should be screen device
+   const radius = 10;
 
    let velocityX = Math.random() * 0.1;
    let velocityY = Math.random() * 0.05;
-   const currentQuaternion = useMemo(() => new Quaternion(), []);
-   const targetQuaternion = useMemo(() => new Quaternion(), []);
 
-   const slerpFactor = 0.9;
+   const numPoints = 25; // Number of points for the curve. More points = smoother curve
+   const frequency = 2; // Determines how many waves are there in the path
+   const points: Vector3[] = [];
+   const randomY = [2, 4, 6, 8];
 
-   // the boundaries should be set as ratios NOT numbers
-   const upperBoundaryX = 10;
-   const upperBoundaryY = 5;
-   const lowerBoundaryX = -upperBoundaryX;
-   const lowerBoundaryY = -upperBoundaryY;
+   const getPoints = (points: Vector3[], radius: number) => {
+      for (let i = 0; i <= numPoints; i++) {
+         const ratio = i / numPoints;
+         const x = Math.sin(ratio) * radius * 3;
+         const y = randomY[Math.floor(Math.random() * randomY.length)] % i;
+         const z = Math.cos(ratio) * Math.abs(radius); // only want it to go away from the screen
+         points.push(new Vector3(x, y, z));
+      }
+      const reversedPoints = [...points].reverse();
+      points.push(...reversedPoints);
+   };
 
-   let previousZ: number = 0;
-   if (birdRef.current) {
-      birdRef.current.rotation.y = Math.PI * 0.5;
-   }
+   const curve = useMemo(() => {
+      getPoints(points, radius);
+      getPoints(points, -radius);
 
-   const r = () => Math.max(0.2, Math.random());
+      // const reversedPoints = [...points].reverse();
+      // points.push(...reversedPoints);
 
-   const lines = useMemo(
-      () =>
-         new Array(count).fill().map((_, index) => {
-            const pos = new Vector3(Math.sin(0) * radius * r(), Math.cos(0) * radius * r(), 0);
-            const points = new Array(30).fill().map((_, index) => {
-               const angle = (index / 20) * Math.PI * 2;
-               return pos
-                  .add(
-                     new Vector3(Math.sin(angle) * radius * r(), Math.cos(angle) * radius * r(), 0)
-                  )
-                  .clone();
-            });
-            const curve = new CatmullRomCurve3(points).getPoints(100);
-            return {
-               curve,
-            };
-         }),
-      [count, radius]
-   );
+      return new CatmullRomCurve3(points);
+   }, []);
 
-   useFrame((state, delta) => {
-      if (!birdRef.current) return;
+   // const points = [
+   //    new Vector3(0, 0, -1),
+   //    new Vector3(5, 3, -3),
+   //    new Vector3(10, 5, -3),
+   //    new Vector3(10, 3, 0),
+   //    new Vector3(-4, 3, 3),
+   //    new Vector3(-3, 2, 5),
+   //    new Vector3(-5, 3, -3),
+   //    new Vector3(10, 5, -3),
+   //    // new Vector3(10, 0, 30),
+   //    // new Vector3(0, 0, 30),
+   //    // new Vector3(-10, 10, 30),
+   //    // new Vector3(-10, 20, 30),
+   //    // new Vector3(0, 30, 30),
+   //    // new Vector3(10, 30, 30),
+   //    // new Vector3(20, 30, 15),
+   //    // new Vector3(10, 30, 10),
+   //    // new Vector3(0, 30, 10),
+   //    // new Vector3(-10, 20, 10),
+   //    // new Vector3(-10, 10, 10),
+   //    // new Vector3(0, 0, 10),
+   //    // new Vector3(10, -10, 10),
+   //    // new Vector3(20, -15, 10),
+   //    // new Vector3(30, -15, 10),
+   //    // new Vector3(40, -15, 10),
+   //    // new Vector3(50, -15, 10),
+   // ];
 
-      theta += delta * Math.random(); // slowing down effect
-      const rand = MathUtils.randFloatSpread(sigma) * Math.random();
+   // const curve = useMemo(() => new CatmullRomCurve3(points), []);
 
-      // squares this so that time spent at +z is more;
-      birdRef.current.position.x += velocityX;
-      birdRef.current.position.z += velocityX;
-      const z = radius * Math.pow(Math.cos((theta * rand) / rand), 2);
+   let targetVelocityX = -velocityX; // The velocity you want to reach
+   let velocityChangeSpeed = 0.1; // How fast the velocity changes, tweak this value
+   let t = 0; // Parameter for traversing the curve
+   const curveSpeed = 0.0005;
 
-      // NO NEED TO CREATE ANOTHER VECTOR3 EVERY TIME!
-      let direction = new Vector3(
-         birdRef.current.position.x - previousPosition.x,
-         0,
-         birdRef.current.position.z - previousPosition.z
-      ).normalize();
+   useFrame(() => {
+      t += curveSpeed;
 
-      if (
-         birdRef.current.position.x > upperBoundaryX ||
-         birdRef.current.position.x < lowerBoundaryX
-      ) {
-         // FOR THIS PART WHY NOT GO AROUND IN A LOOP(?)
-         birdRef.current.rotation.y = Math.PI * 1.5;
-         velocityX = -velocityX; // Reverse the x velocity
+      if (t >= 10) {
+         t = 0;
+
+         // If at boundary, reverse direction and create a new curve
+         targetVelocityX = -velocityX;
+         velocityX += (targetVelocityX - velocityX) * velocityChangeSpeed;
+
+         // Generate a new set of points and curve
+         points.reverse(); // Reversing so the bird moves back on the opposite direction
       }
 
-      if (!direction.equals(new Vector3(0, 0, 0))) {
-         // if (z < previousZ) {
-         //    // The bird is moving backward, rotate it to face away from the screen
-         //    birdRef.current.rotation.y = Math.PI; // 180 degrees in radians
-         // } else if (z > previousZ) {
-         //    // The bird is moving forward, rotate it to face the screen
-         //    birdRef.current.rotation.y = 0;
-         // }
+      birdRef.current.position.copy(curve.getPoint(t));
 
-         birdRef.current.lookAt(birdRef.current.position.clone().add(direction));
-      }
-
-      // birdRef.current.position.x += velocityX;
-      // birdRef.current.position.y += 0.05 * Math.sin(theta * 0.001);
-
-      // birdRef.current.position.z = z;
-
-      previousZ = z;
-      previousPosition.set(
-         birdRef.current.position.x,
-         birdRef.current.position.y,
-         birdRef.current.position.z
-      );
-
-      // setInterval and switch every time wrapped around in a function(?)
-      // setInterval(() => changePosition(), 1500);
-      // const currentPosition = birdRef.current.position;
+      // For rotation, take the tangent to the curve as the direction of movement
+      let tangent = curve.getTangent(t).normalize();
+      birdRef.current.lookAt(birdRef.current.position.clone().add(tangent));
    });
+
+   // useFrame((state, delta) => {
+   //    if (!birdRef.current) return;
+
+   //    theta += delta * Math.random(); // slowing down effect
+   //    const rand = MathUtils.randFloatSpread(sigma) * Math.random();
+
+   //    // squares this so that time spent at +z is more;
+   //    birdRef.current.position.x += velocityX;
+   //    birdRef.current.position.z += velocityX;
+   //    const z = radius * Math.pow(Math.cos((theta * rand) / rand), 2);
+
+   //    // NO NEED TO CREATE ANOTHER VECTOR3 EVERY TIME!
+   //    let direction = new Vector3(
+   //       birdRef.current.position.x - previousPosition.x,
+   //       0,
+   //       birdRef.current.position.z - previousPosition.z
+   //    ).normalize();
+
+   //    if (
+   //       birdRef.current.position.x > upperBoundaryX ||
+   //       birdRef.current.position.x < lowerBoundaryX
+   //    ) {
+   //       // FOR THIS PART WHY NOT GO AROUND IN A LOOP(?)
+   //       birdRef.current.rotation.y = Math.PI * 1.5;
+   //       velocityX += (targetVelocityX - velocityX) * velocityChangeSpeed;
+   //    }
+
+   //    if (!direction.equals(new Vector3(0, 0, 0))) {
+   //       birdRef.current.lookAt(birdRef.current.position.clone().add(direction));
+   //    }
+
+   //    // birdRef.current.position.x += velocityX;
+   //    // birdRef.current.position.y += 0.05 * Math.sin(theta * 0.001);
+
+   //    // birdRef.current.position.z = z;
+
+   //    previousZ = z;
+   //    previousPosition.set(
+   //       birdRef.current.position.x,
+   //       birdRef.current.position.y,
+   //       birdRef.current.position.z
+   //    );
+
+   //    // setInterval and switch every time wrapped around in a function(?)
+   //    // setInterval(() => changePosition(), 1500);
+   //    // const currentPosition = birdRef.current.position;
+   // });
 
    // have a target. The target is the center
    // when touching the seagull it will increase its velocity and move towards the sea direction
 
    return (
-      <group position={[-radius * 2, radius * 2, 0]}>
-         <primitive
-            points={lines}
-            ref={birdRef}
-            position={[0, 0, 0]}
-            object={gltf.scene}
-            rotation={[0, 0, 0]}
-         />
+      <group position={[0, 0, 5]}>
+         <primitive ref={birdRef} position={[0, 0, 0]} object={gltf.scene} rotation={[0, 0, 0]} />
          {/* <mesh ref={birdRef} position={position} rotation={}>
             <meshBasicMaterial color={'red'} />
             <boxGeometry args={[2, 3]} />
@@ -148,5 +177,11 @@ const Seagull = ({ position, count = 10 }: { position: Vector3; count?: number }
 
 export default Seagull;
 
-// 1) download the .png file
-// 2) then
+// export const Sample = () => {
+//    const line = useMemo(() => new LineBasicMaterial, []);
+//    const material = useMemo(() => new Geometry, []);
+//    return (
+//       <mesh>
+//       </mesh>
+//    )
+// }
