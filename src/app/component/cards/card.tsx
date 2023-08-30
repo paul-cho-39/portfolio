b4ae8a3922d1e31e96b633bba39d84ee82761c26
Paul Cho <chosung2loud@gmail.com>
2023-08-29 17:38:22 -0700

    adding scroll animation to cards as well

--- src/app/component/cards/card.tsx
+++ src/app/component/cards/card.tsx
@@ -6,6 +6,7 @@ import { useDisableBreakPoints } from '@/app/library/hooks/useDisableBreakPoints
 
 import { ArrowTopRightOnSquareIcon } from '@heroicons/react/24/outline';
 import { SourceCode } from './contents';
+import { motion, Variants } from 'framer-motion';
 
 export interface ProjectsProps {
    title: string;
@@ -45,11 +46,38 @@ const Cards = ({ projects }: ProjectCardsProps) => {
       if (!el.current) return;
    }, []);
 
+   // TODO:
+   // a) if it is flex it should come out a bit differently
+   // b) image should come out from the bottom to up
+   // c) card title comes from left to right (depending on flex)
+   // d) card statement opacity
+   const cardVariants: Variants = {
+      offscreen: {
+         y: -200,
+         scale: 0.9,
+         opacity: 0.5,
+      },
+      onscreen: {
+         x: 0,
+         scale: 1,
+         opacity: 1,
+         transition: {
+            type: 'spring',
+            bounce: 0.2,
+            duration: 1,
+         },
+      },
+   };
+
    return (
       <>
          {projects.map((project: ProjectsProps, index: number) => (
-            <article
+            <motion.article
                key={index}
+               initial='offscreen'
+               whileInView='onscreen'
+               variants={cardVariants}
+               // viewport={{ once: false, amount: 0.8 }}
                className={classNames(
                   isOdd(index) ? 'lg:flex-row-reverse ' : 'lg:flex-row',
                   'xl:px-20',
@@ -63,6 +91,7 @@ const Cards = ({ projects }: ProjectCardsProps) => {
                >
                   {/* <Link className='z-0 lg:pointer-events-none' href={'/'}> */}
                   <div
+                     variants={cardVariants}
                      className={classNames(
                         isOdd(index) ? 'lg:flex-row-reverse ' : 'lg:flex-row',
                         'flex px-6 py-2'
@@ -140,85 +169,135 @@ const Cards = ({ projects }: ProjectCardsProps) => {
                      aria-label={`View source code for ${project.title}`}
                   />
                )}
-            </article>
+            </motion.article>
          ))}
       </>
    );
 };
 
-export default Cards;
+// const Cards = ({ projects }: ProjectCardsProps) => {
+//    const el = useRef<HTMLDivElement>(null);
+//    const isOdd = (idx: number) => idx % 2 !== 0;
 
-{
-   /* contents */
-}
-//  {projects.map((project: ProjectsProps, index: number) => (
-//     <div
-//        key={index}
-//        style={{
-//         paddingLeft:
-//        }}
-//        className={classNames(
-//           isOdd(index) ? 'lg:flex-row-reverse ' : 'lg:flex-row',
-//         //   repositionByIndex(index),
-//         //   //   'pl-10',
-//           'bg-red-500 mx-auto flex max-w-5xl flex-col items-center gap-x-16 gap-y-10  lg:items-stretch group transform transition-all duration-300 ease-in-out'
-//        )}
-//     >
-//        <div
-//           className={classNames(
-//              isOdd(index) ? 'lg:-ml-6' : 'lg:-mb-8',
-//              '-mt-8 w-full max-w-2xl lg:w-96 lg:flex-none'
-//           )}
-//        >
-//           <div className='md:relative aspect-[5/1] lg:h-full md:-mx-8 lg:mx-0 lg:aspect-square lg:hover:drop-shadow-2xl lg:hover:shadow-gray-800 group-hover:-translate-y-3 lg:group-hover:translate-y-0'>
-//              <Link href={'/'}>
-//                 <img
-//                    className='absolute inset-0 -left-14 h-full w-full rounded-2xl object-cover opacity-30 lg:opacity-100 lg:shadow-2xl lg:h-[50%] lg:hover:opacity-90 lg:hover:-translate-y-3'
-//                    src={project.image}
-//                    alt={project.title}
-//                 />
-//              </Link>
-//           </div>
-//        </div>
-//        {/* ... */}
-//        <div
-//           className={classNames(
-//              isOdd(index) ? 'md:-top-24 md:px-6' : 'md:-top-16 md:px-4',
-//              'relative -top-8 sm:-top-14 z-20 w-full max-w-2xl lg:py-24 xl:mx-2 xl:px-20 xl:flex-auto group-hover:-translate-y-3 lg:group-hover:translate-y-0'
-//           )}
-//        >
-//           {/* //    className='relative -top-8 sm:-top-14 md:-top-16 z-20 w-full max-w-2xl lg:py-24 lg:px-4 xl:mx-2 xl:px-20 xl:flex-auto group-hover:-translate-y-3 lg:group-hover:translate-y-0'> */}
-//           <div className='pb-4 '>
-//              <h3 className='font-serif text-2xl'>{project.title}</h3>
-//           </div>
-//           {/* description */}
-//           <p className='font-medium mb-6 lg:mb-4 tracking-wide leading-5 md:leading-6 lg:leading-7'>
-//              {project.description}
-//           </p>
-//           {/* ... */}
-//           <div className='flex flex-row items-start'>
-//              <ul className='justify-start gap-x-2 overflow-auto' role='listitem'>
-//                 {project.badge.map((badge, badgeIndex) => (
-//                    <li
-//                       aria-label={badge}
-//                       className='inline-flex items-center rounded-full bg-gray-100 px-2 py-1 text-xs font-medium text-blue-700'
-//                       key={badgeIndex}
-//                    >
-//                       {badge}
-//                    </li>
-//                 ))}
-//              </ul>
-//              <div className='flex flex-row items-start justify-start py-4 gap-x-1 lg:gap-x-2'>
-//                 <Link
-//                    aria-label={`View source code for ${project.title} on GitHub`}
-//                    href={'/'}
-//                 >
-//                    {/* link here */}
-//                    <GithubIcon className='' width={25} height={25} />
-//                 </Link>
-//              </div>
-//           </div>
-//           {/* ... */}
-//        </div>
-//     </div>
-//  ))}
+//    const repositionByIndex = (idx: number) => {
+//       const maxStep = projects.length; // maximum step boundary
+//       const stepSize = 5; // step size in pixels
+//       let step;
+
+//       if (idx <= maxStep) {
+//          step = idx * stepSize;
+//       } else {
+//          step = (maxStep - (idx - maxStep)) * stepSize;
+//       }
+//       //   return `pl-${step}`;
+//       return step + 'rem';
+//    };
+
+//    const isDisabled = useDisableBreakPoints();
+
+//    useEffect(() => {
+//       if (!el.current) return;
+//    }, []);
+
+//    return (
+//       <>
+//          {projects.map((project: ProjectsProps, index: number) => (
+//             <article
+//                key={index}
+//                className={classNames(
+//                   isOdd(index) ? 'lg:flex-row-reverse ' : 'lg:flex-row',
+//                   'xl:px-20',
+//                   'mx-auto flex w-full flex-col items-center gap-x-16 mb-6 lg:mb-0 lg:items-stretch'
+//                )}
+//             >
+//                <div
+//                   className={classNames(
+//                      'group transform transition-all duration-300 ease-in-out lg:mb-24 xl:mb-28'
+//                   )}
+//                >
+//                   {/* <Link className='z-0 lg:pointer-events-none' href={'/'}> */}
+//                   <div
+//                      className={classNames(
+//                         isOdd(index) ? 'lg:flex-row-reverse ' : 'lg:flex-row',
+//                         'flex px-6 py-2'
+//                      )}
+//                   >
+//                      <div
+//                         className={classNames(
+//                            //    isOdd(index) ? 'lg:-ml-6' : 'lg:-mb-8',
+//                            //    '                           bg-gray-700',
+//                            '-mt-8 max-w-2xl lg:w-96 lg:flex-none '
+//                         )}
+//                      >
+//                         <div className='lg:relative top-2 lg:h-full md:-mx-8 lg:mx-0 lg:hover:drop-shadow-2xl lg:hover:shadow-gray-800 lg:group-hover:translate-y-0'>
+//                            <Link
+//                               className='lg:pointer-events-auto lg:cursor-pointer lg:hover:opacity-90 lg:hover:-translate-y-3 transition-all duration-300 '
+//                               href={'/'}
+//                            >
+//                               <img
+//                                  className='absolute inset-0  h-full w-full rounded-2xl object-cover opacity-30 lg:opacity-100 lg:shadow-2xl lg:h-[70%] xl:h-[85%] lg:hover:-translate-y-3 transition-all duration-300'
+//                                  src={project.image}
+//                                  alt={project.title}
+//                               />
+//                            </Link>
+//                         </div>
+//                      </div>
+
+//                      {/* CONTENTS HERE */}
+//                      <div
+//                         className={classNames(
+//                            'px-4 md:px-20 lg:max-w-2xl lg:-px-6 xl:max-w-xl ',
+//                            'flex-grow items-center justify-start align-top self-start '
+//                         )}
+//                      >
+//                         <div className='pb-4'>
+//                            <h3 className='font-serif text-2xl'>{project.title}</h3>
+//                         </div>
+
+//                         {/* description */}
+//                         <p className='font-medium mb-6 lg:mb-4 tracking-wide leading-5 md:leading-6 lg:leading-7'>
+//                            {project.description}
+//                         </p>
+//                         {/* ... */}
+//                         <ul
+//                            className='flex flex-row items-start justify-start gap-x-2 overflow-auto'
+//                            role='listitem'
+//                         >
+//                            {project.badge.map((badge, badgeIndex) => (
+//                               <li
+//                                  aria-label={badge}
+//                                  className='inline-flex items-center rounded-full bg-gray-100 px-2 py-1 text-xs font-medium text-blue-800'
+//                                  key={badgeIndex}
+//                               >
+//                                  {badge}
+//                               </li>
+//                            ))}
+//                         </ul>
+//                         <div className='flex flex-row items-start justify-start my-2 lg:hidden'>
+//                            <Link
+//                               className='my-2'
+//                               aria-label={`View source code for ${project.title}`}
+//                               href={'/'}
+//                            >
+//                               {/* link here */}
+//                               <GithubIcon className='block' width={25} height={25} />
+//                            </Link>
+//                         </div>
+//                      </div>
+//                   </div>
+//                </div>
+//                {/* numbering the project(?) / other details? */}
+//                {isDisabled && (
+//                   <SourceCode
+//                      isOdd={isOdd}
+//                      index={index}
+//                      aria-label={`View source code for ${project.title}`}
+//                   />
+//                )}
+//             </article>
+//          ))}
+//       </>
+//    );
+// };
+
+export default Cards;
--- src/app/component/layouts/frontPageLayout.tsx
+++ src/app/component/layouts/frontPageLayout.tsx
@@ -9,13 +9,12 @@ import {
    motion,
    useMotionValueEvent,
    useMotionValue,
+   useInView,
 } from 'framer-motion';
+import { lerp } from 'three/src/math/MathUtils';
+import classNames from '@/app/library/helper';
 
 const OPACITY_THRESHOLD = 0.2;
-
-const lerp = (a: number, b: number, n: number) => {
-   return (1 - n) * a + n * b;
-};
 
 const FronPageLayout = ({ children }: { children: React.ReactNode }) => {
    // if detecting scroll, change the position to fixed and then change it to
@@ -36,36 +35,40 @@ const FronPageLayout = ({ children }: { children: React.ReactNode }) => {
 const FrontPage = () => {
    const ref = useRef<HTMLSelectElement>(null);
 
+   // TODO: set margin or play with this number
+
+   // whenever scrolling up (last position)
+   // save the last position and see if there will be difference
+
+   const isInView = useInView(ref, {
+      margin: '-250px',
+   });
+
+   useEffect(() => {
+      console.log('is it in view?: ', isInView);
+   }, [isInView]);
+
    const controls = useAnimation();
    const { scrollY } = useScroll();
    const opacity = useMotionValue(1);
-
    let currentOpacity = 1;
+
    useMotionValueEvent(scrollY, 'change', (latest) => {
-      if (ref.current) {
-         const sectionHeight = ref.current?.scrollHeight;
-         console.log('Latest Scroll:', latest);
-         console.log('Section Height:', sectionHeight);
-         console.log('******************');
-         const scrollPercentage = Math.max((sectionHeight - latest) / sectionHeight, 0);
+      if (!ref.current || !isInView) return;
 
-         const targetOpacity = Math.pow(scrollPercentage, 1.5);
+      const sectionHeight = ref.current?.scrollHeight;
+      const scrollPercentage = Math.max((sectionHeight - latest) / sectionHeight, 0);
 
-         console.log('Scroll Percentage:', scrollPercentage);
-         console.log('-------------------');
+      const targetOpacity = Math.pow(scrollPercentage, 1.5);
 
-         currentOpacity = lerp(currentOpacity, targetOpacity, 0.25);
+      currentOpacity = lerp(currentOpacity, targetOpacity, 0.2);
 
-         opacity.set(currentOpacity);
+      opacity.set(currentOpacity);
 
-         console.log('Target Opacity:', targetOpacity);
-         console.log('******************');
-
-         if (currentOpacity <= OPACITY_THRESHOLD) {
-            controls.start({ display: 'none' });
-         } else {
-            controls.start({ display: 'block' });
-         }
+      if (currentOpacity <= OPACITY_THRESHOLD) {
+         controls.start({ display: 'none' });
+      } else {
+         controls.start({ display: 'block' });
       }
    });
 
@@ -76,7 +79,11 @@ const FrontPage = () => {
                style={{ opacity: opacity }}
                animate={controls}
                initial={{ display: 'block' }}
-               className='fixed inset-0 sky-fade-gradient -z-10'
+               className={classNames(
+                  isInView ? 'fixed' : 'invisible',
+                  'inset-0 sky-fade-gradient -z-10'
+               )}
+               // className='fixed inset-0 sky-fade-gradient -z-10'
             >
                <WindowCanvas />
             </motion.div>
