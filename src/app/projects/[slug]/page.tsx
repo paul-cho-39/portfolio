e670709734dd457c86f5a5af0dbf63f1a0b60d21
Paul Cho <chosung2loud@gmail.com>
2023-12-04 15:27:25 -0800

    added for mdx files to be loaded dynamically

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,8 +1,42 @@
+'use client';
+import { useState, useEffect, cloneElement } from 'react';
+// import Files from './../../posts/projects/'
+
 export default function ProjectsPage(props: any) {
    //
    const slug = props.params.slug;
 
-   console.log('the slugs are');
+   //    const [MDXContent, setMDXContent] = useState<JSX.Element | undefined>(undefined);
+   const [MDXContent, setMDXContent] = useState<JSX.Element | null>(null);
+   const [error, setError] = useState(false);
 
-   return <div>{slug}</div>;
+   useEffect(() => {
+      const loadMDX = async () => {
+         try {
+            // dynamically import the MDX file based on the slug
+            const content = await import(`./../../posts/projects/${slug}.mdx`);
+            // console.log('the content is: ', content.default());
+
+            setMDXContent(content.default());
+         } catch (err) {
+            // handling error for fallback component
+            setError(true);
+            console.error('Error loading MDX file:', err);
+         }
+      };
+
+      loadMDX();
+   }, [slug]);
+
+   if (error) {
+      return (
+         <div className='text-2xl dark:text-gray-300'>Cannot find the page you are looking for</div>
+      );
+   }
+
+   if (!MDXContent) return <div>Loading...</div>;
+
+   return <div>{MDXContent && cloneElement(MDXContent, { key: slug })}</div>;
 }
+
+// dynamic approach(?)
