c712bdddd56dcae42ae28b226a241a04e4b686be
Paul Cho <chosung2loud@gmail.com>
2023-12-13 15:18:24 -0800

    adding click functionalities to ripple button

--- src/app/component/buttons/rippleEffectButton.tsx
+++ src/app/component/buttons/rippleEffectButton.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useRef, useState } from 'react';
 interface RippleEffectButtonProps {
    name: string;
    onClick: () => void;
+   createRippleOnClick?: boolean;
    rippleSize?: number;
    rippleColor?: React.CSSProperties['backgroundColor'];
    rippleTime?: number;
@@ -16,20 +17,35 @@ type RippleEffect = {
    style: React.CSSProperties;
 };
 
+type RippleParams = {
+   event?: React.MouseEvent<HTMLButtonElement>;
+   mouseX?: number;
+   mouseY?: number;
+};
+
 const DEFAULT_SIZE = 50;
 const DEFAULT_TIME = 800;
 const DEFAULT_RIPPLE_INTERVAL = 500;
-const OFFSET_BY = 40;
 
 /**
  * Sets ripple effect with customizable color and size when hovered over.
  * The effect will persist as long as the user hovers over the button.
- * @param {object} {name, onClick, rippleSize, rippleColor, rippleTime, rippleInterval, className}
- * @returns JSX.Element
+ * The button has an option to create ripple effect when clicked
+ * @param {object} props
+ * @param {string} props.name - The text to display on the button.
+ * @param {Function} props.onClick - Function to call when the button is clicked.
+ * @param {boolean} [props.createRippleOnClick] - Whether to create a ripple effect on click.
+ * @param {number} [props.rippleSize=DEFAULT_SIZE] 
+ * @param {string} [props.rippleColor='blue'] 
+ * @param {number} [props.rippleTime=DEFAULT_TIME] - Duration of the ripple effect.
+ * @param {number} [props.rippleInterval=DEFAULT_RIPPLE_INTERVAL] - Interval between ripple effects when hovered.
+ * @param {string} [props.className]
+
  */
 const RippleEffectButton = ({
    name,
    onClick,
+   createRippleOnClick,
    rippleSize = DEFAULT_SIZE,
    rippleColor = 'blue',
    rippleTime,
@@ -42,14 +58,26 @@ const RippleEffectButton = ({
    const buttonRef = useRef<HTMLButtonElement>(null);
    const counterRef = useRef(0);
 
-   useEffect(() => {
-      console.log('BEING HOVERED');
+   //    helper function
+   const getBtnAttr = (button: HTMLButtonElement) => {
+      const rect = button.getBoundingClientRect();
+      const diameter = Math.max(rect.width, rect.height);
+      const radius = diameter / 2;
 
+      return { rect, diameter, radius };
+   };
+
+   useEffect(() => {
       let interval: NodeJS.Timer;
       if (mousePosition.x !== 0 && mousePosition.y !== 0) {
          interval = setInterval(() => {
             // as long as the mouse position is set newRipple should not return null
-            const newRipple = createRipple(mousePosition.x, mousePosition.y) as RippleEffect;
+            const newRipple = createRipple(
+               buttonRef.current,
+               mousePosition.x,
+               mousePosition.y
+            ) as RippleEffect;
+
             setRipples((prev) => [...prev, newRipple]);
 
             setTimeout(() => {
@@ -66,17 +94,17 @@ const RippleEffectButton = ({
       // eslint-disable-next-line react-hooks/exhaustive-deps
    }, [mousePosition, ripples]);
 
-   const createRipple = (mouseX: number, mouseY: number) => {
-      const button = buttonRef.current;
+   const createRipple = (button: HTMLButtonElement | null, mouseX: number, mouseY: number) => {
+      //   const button = buttonRef.current;
       if (button) {
-         const rect = buttonRef.current?.getBoundingClientRect();
+         const { rect, diameter } = getBtnAttr(button);
+         //  const pos = getRipplePos(params, rect);
+         //  if 'rippleSize' is assigned and the value is more than the diameter of the button
+         //  it sets the maximum size to the diameter
+         const size = Math.min(diameter, rippleSize);
 
-         const x = mouseX - rect.left;
-         const y = mouseY - rect.top;
-         //  if 'rippleSize' is assigned and if over minit sets the maximum size to either height/width
-         const maxSize = Math.max(button.clientHeight, button.clientWidth);
-         const size = Math.min(maxSize, rippleSize);
-         const offset = (size - OFFSET_BY) / 2;
+         const x = mouseX - rect.left - size / 2;
+         const y = mouseY - rect.top - size / 2;
 
          return {
             key: `${Date.now()}-${counterRef.current++}`,
@@ -105,10 +133,30 @@ const RippleEffectButton = ({
       setMousePosition({ x: 0, y: 0 });
    };
 
+   const handlePress = (event: React.MouseEvent<HTMLButtonElement>) => {
+      // since it will leave the mouse(?)
+      if (createRippleOnClick) {
+         const button = event.currentTarget;
+         const newRipple = createRipple(button, event.clientX, event.clientY) as RippleEffect;
+
+         setRipples((prev) => [...prev, newRipple]);
+
+         // cleanup ripple after animation
+         setTimeout(() => {
+            setRipples((prev) => prev.filter((ripple) => ripple.key !== newRipple.key));
+         }, 500);
+      }
+
+      // cleaning up ripple effect
+      handleMouseLeave();
+
+      onClick();
+   };
+
    return (
       <button
          ref={buttonRef}
-         onClick={onClick}
+         onClick={handlePress}
          onMouseEnter={handleMouseEnter}
          onMouseLeave={handleMouseLeave}
          className={classNames(
--- src/app/component/description/contactDescription.tsx
+++ src/app/component/description/contactDescription.tsx
@@ -9,10 +9,12 @@ const ContactDescription = ({ onClick }: ContactDescriptionProps) => {
    const handleEmailClick = () => {
       const emailAddress = 'chosung2loud@gmail.com';
       window.location.href = `mailto:${emailAddress}`;
+
+      // console.log('PRESSED HERE');
    };
 
    return (
-      <div className='w-full mx-auto flex bg-red-500'>
+      <div className='w-full mx-auto flex'>
          <div className='flex w-full flex-col items-center justify-center'>
             <p className='text-lg mb-4 opacity-95 text-center'>
                {"Interested in collaborating? I'm open to freelance and project opportunities!"}
@@ -20,6 +22,7 @@ const ContactDescription = ({ onClick }: ContactDescriptionProps) => {
             <RippleEffectButton
                name='Drop a Message'
                className='sky-fade-gradient'
+               createRippleOnClick={true}
                onClick={handleEmailClick}
                rippleColor='dodgerblue'
             />
@@ -30,14 +33,12 @@ const ContactDescription = ({ onClick }: ContactDescriptionProps) => {
 
 const ContactHeader = () => {
    return (
-      <div className='sky-fade-gradient'>
-         <div className='pt-12 text-center my-2 lg:my-4'>
-            <h2 className='font-serif sm:text-3xl font-bold lg:text-5xl'>
-               <span>{"Let's"}</span>
-               <br />
-               <span className='block'>{'Connect'}</span>
-            </h2>
-         </div>
+      <div className='pt-12 text-center my-2 lg:my-4'>
+         <h2 className='font-serif sm:text-3xl font-bold lg:text-5xl'>
+            <span>{"Let's"}</span>
+            <br />
+            <span className='block'>{'Connect'}</span>
+         </h2>
       </div>
    );
 };
