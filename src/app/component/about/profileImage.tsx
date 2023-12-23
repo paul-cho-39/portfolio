c64c16a0244e6e63d9e7f29373be79dc2d2f0bc1
Paul Cho <chosung2loud@gmail.com>
2023-12-23 14:34:49 -0800

    adding photos and descriptions

--- src/app/component/about/profileImage.tsx
+++ src/app/component/about/profileImage.tsx
@@ -5,7 +5,7 @@ const ProfileImage = () => {
       <div className='sm:shadow-md lg:shadow-xl rounded-lg overflow-hidden inline-block'>
          <Image
             className='w-full overflow-hidden h-auto '
-            src='/images/about_me.jpg'
+            src='/images/paul.jpg'
             alt='Photo of Paul Cho'
             width={75}
             height={75}
